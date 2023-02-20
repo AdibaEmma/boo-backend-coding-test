05697f48058386334739d2bbf7a21b97773a5aa4
Emmanuel Adiba <eabaagah@gmail.com>
2023-02-20 14:59:12 +0000

    setup config

---
+++ server/config/common.config.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const SERVER_PORT = process.env.SERVER_PORT
+  ? Number(process.env.SERVER_PORT)
+  : 3001;
+
+const DB_NAME = process.env.DEV_DB_NAME || ''
+const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@renteasecluster.fkbqbzj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+export const config = {
+  mongo: {
+    uri: MONGO_URI,
+    options: {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      autoIndex: false,
+      useFindAndModify: false,
+    },
+  },
+  server: {
+    port: SERVER_PORT,
+  },
+};
