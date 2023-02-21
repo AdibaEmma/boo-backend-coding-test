44d6241a00991689576c97774765ec7c0b7dd9ee
Emmanuel Adiba <eabaagah@gmail.com>
2023-02-21 19:22:40 +0000

    update user test

--- server/test/services/user.test.js
+++ server/test/services/user.test.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { createUser } from "../../src/services/users/createUser";
+import { getUser } from "../../src/services/users/getUser";
 
 const mongoServer = await MongoMemoryServer.create();
 
@@ -29,6 +30,31 @@ describe("User Service", () => {
       expect(newUser.lastName).toBe(userDetails.lastName);
       expect(newUser.email).toBe(userDetails.email);
       expect(newUser.birthdate).toEqual(new Date(userDetails.birthdate));
+    });
+  });
+
+  describe("getUser", () => {
+    it("should return the user if found", async () => {
+        const user = {
+          firstName: "Brandon",
+          lastName: "Smith",
+          email: "smithg@gmail.com",
+          birthdate: "1992-03-04",
+        };
+      const filterQuery = { email: "smithg@gmail.com" };
+
+      const result = await getUser(filterQuery);
+
+      expect(result.email).toEqual(user.email);
+      expect(result.firstName).toEqual(user.firstName);
+    });
+
+    it("should return null if user not found", async () => {
+      const filterQuery = { email: "notfound@example.com" };
+
+      const result = await getUser(filterQuery);
+
+      expect(result).toBeNull();
     });
   });
 });
