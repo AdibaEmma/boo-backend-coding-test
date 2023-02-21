ad87372688bdccb27ff839393262c8abef6b9832
Emmanuel Adiba <eabaagah@gmail.com>
2023-02-21 16:33:17 +0000

    setup test and update directory

---
+++ server/src/services/routes/user.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+const router = Router();
+
+import { createUserAccount } from "../../controllers/userController.js";
+import {
+  postComment,
+  returnAllCommentsByUSer,
+} from "../../controllers/commentController.js";
+
+import {
+  createUserValidationRules,
+  validateUser,
+} from "../../middlewares/validators/userValidator.js";
+
+export const userRoutes = () => {
+  router.post(
+    "/",
+    createUserValidationRules(),
+    validateUser,
+    createUserAccount
+  );
+
+  router.get("/:userId");
+
+  router.post("/:userId/comments", postComment);
+
+  router.get("/:userId/comments", returnAllCommentsByUSer);
+
+  return router;
+};
